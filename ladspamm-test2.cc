a29729445f3303947a38d23906e8e1f6c0231839
Florian Paul Schmidt <mista.tapas@gmx.net>
2023-02-28 09:35:07 +0100

    catch the error

--- ladspamm-test2.cc
+++ ladspamm-test2.cc
@@ -10,24 +10,28 @@ int main()
 {
 	using namespace ladspamm1;
 
-	library l("/usr/lib/ladspa/ladspa.m.osc.plugins1.so");
-	plugin_instance i(l.plugins[0], 1000);
-
-	std::vector<float> freq(1000, 1);
-	std::vector<float> phase(1000, 0);
-	std::vector<float> trigger(1000, 0);
-	std::vector<float> out(1000, 0);
-
-	i.connect_port(0, &freq[0]);
-	i.connect_port(1, &phase[0]);
-	i.connect_port(2, &trigger[0]);
-	i.connect_port(3, &out[0]);
-
-	i.run(1000);
-
-	for (size_t frame = 0; frame < 1000; ++frame)
-	{
-		std::cout << out[frame] << std::endl;
-	}	
+  try {
+		library l("/usr/lib/ladspa/ladspa.m.osc.plugins1.so");
+		plugin_instance i(l.plugins[0], 1000);
+	
+		std::vector<float> freq(1000, 1);
+		std::vector<float> phase(1000, 0);
+		std::vector<float> trigger(1000, 0);
+		std::vector<float> out(1000, 0);
+	
+		i.connect_port(0, &freq[0]);
+		i.connect_port(1, &phase[0]);
+		i.connect_port(2, &trigger[0]);
+		i.connect_port(3, &out[0]);
+	
+		i.run(1000);
+	
+		for (size_t frame = 0; frame < 1000; ++frame)
+		{
+			std::cout << out[frame] << std::endl;
+		}	
+	} catch (std::runtime_error &e) {
+		std::cout << "Error: " << e.what() << std::endl;
+	}
 }
 
